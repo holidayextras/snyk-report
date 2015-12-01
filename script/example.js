442ae9bd6ce9bb4afec3ed8211819b8be26ba834
Mark Terry <mark.terry@holidayextras.com>
2015-12-01 01:48:26 +0000

    updated output using colour module

--- script/example.js
+++ script/example.js
@@ -1,9 +1,13 @@
+#!/usr/bin/env node
 'use strict';
 
 var report = require('../lib/report');
 
-var targetDir = process.env.REPO_DIR || __dirname;
+var targetDir = process.env.REPO_DIR || '.';
 report(targetDir, function(err, output) {
-  if (err) throw new Error(err);
-  console.log(output);
+  if (err) {
+    console.log(err);
+    process.exit(1); // eslint-disable-line no-process-exit
+  }
+  console.log(output.text);
 });
