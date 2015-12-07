dac33e6ae988dcef37b76a1bf046221457c26f96
Mark Terry <mark.terry@holidayextras.com>
2015-12-07 23:22:43 +0000

    move test coverage

--- test/unit/report.js
+++ test/unit/report.js
@@ -51,6 +51,66 @@ context('Report', function() {
 
   });
 
+  describe('_createSummary()', function() {
+
+    var summary;
+
+    context('with issues', function() {
+
+      beforeEach(function() {
+        var grouped = {
+          high: [
+           {},
+           {},
+           {}
+          ],
+          medium: [
+           {},
+           {}
+          ],
+          low: [
+            {}
+          ]
+        };
+        summary = Report._createSummary(grouped);
+      });
+
+      it('counts high issues correctly', function() {
+        expect(summary.high).to.equal(3);
+      })
+
+      it('counts medium issues correctly', function() {
+        expect(summary.medium).to.equal(2);
+      })
+
+      it('counts high issues correctly', function() {
+        expect(summary.low).to.equal(1);
+      })
+
+    });
+
+    context('without issues', function() {
+
+      beforeEach(function() {
+        summary = Report._createSummary({});
+      });
+
+      it('counts high issues correctly', function() {
+        expect(summary.high).to.equal(0);
+      })
+
+      it('counts medium issues correctly', function() {
+        expect(summary.medium).to.equal(0);
+      })
+
+      it('counts high issues correctly', function() {
+        expect(summary.low).to.equal(0);
+      })
+
+    });
+
+  });
+
   describe('_formatSummary()', function() {
 
     var testData;
@@ -65,15 +125,15 @@ context('Report', function() {
         }
       });
 
-      it('counts high issues correctly', function() {
+      it('formats high issues correctly', function() {
         expect(Report._formatSummary(testData)).to.match(/HIGH\: 1/);
       });
 
-      it('counts medium issues correctly', function() {
+      it('formats medium issues correctly', function() {
         expect(Report._formatSummary(testData)).to.match(/MEDIUM\: 2/);
       });
 
-      it('counts low issues correctly', function() {
+      it('formats low issues correctly', function() {
         expect(Report._formatSummary(testData)).to.match(/LOW\: 3/);
       });
 
@@ -105,82 +165,4 @@ context('Report', function() {
 
   });
 
-  describe.skip('_formatOutput', function() {
-    var testData = {
-      module1: {
-        low: [1],
-        medium: [2],
-        high: [3]
-      },
-      module2: {
-        low: [4],
-        medium: [5],
-        high: [6]
-      }
-    };
-
-    beforeEach(function() {
-      sinon.stub(Report, '_reportLevels').returns('_reportLevels');
-      sinon.stub(Report, '_reportSummary').returns('_reportSummary');
-    });
-
-    afterEach(function() {
-      Report._reportLevels.restore();
-      Report._reportSummary.restore();
-    });
-
-    it('returns the expected data', function() {
-      expect(Report._formatOutput(testData)).to.equal('\x1b[1mmodule1\x1b[22m\n_reportLevels\x1b[0m\x1b[1mmodule2\x1b[22m\n_reportLevels\x1b[0m_reportSummary');
-    });
-  });
-
-  describe.skip('_reportLevels', function() {
-    var testData = {
-      low: 'low',
-      medium: 'medium',
-      high: 'high'
-    };
-
-    beforeEach(function() {
-      sinon.stub(Report, '_formatItems');
-      Report._reportLevels(testData);
-    });
-
-    afterEach(function() {
-      Report._formatItems.restore();
-    });
-
-    it('calls _formatItems with expected parameters', function() {
-      expect(Report._formatItems).to.be.calledWith('low', Report._colourCodes.low);
-      expect(Report._formatItems).to.be.calledWith('medium', Report._colourCodes.medium);
-      expect(Report._formatItems).to.be.calledWith('high', Report._colourCodes.high);
-    });
-  });
-
-  describe.skip('_formatItems', function() {
-    var testData = [
-      {
-        title: 'title1',
-        path: 'path1'
-      },
-      {
-        title: 'title2',
-        path: 'path2'
-      }
-    ];
-    var colourCode = 'colourCode';
-
-    beforeEach(function() {
-      sinon.spy(Report, '_formatItems');
-      Report._formatItems(testData, colourCode);
-    });
-
-    afterEach(function() {
-      Report._formatItems.restore();
-    });
-
-    it('transforms the data correctly', function() {
-      expect(Report._formatItems(testData, colourCode)).to.equal('colourCodetitle1\ncolourCodeCall `snyk test path1` for more info\n\ncolourCodetitle2\ncolourCodeCall `snyk test path2` for more info\n\n');
-    });
-  });
 });
